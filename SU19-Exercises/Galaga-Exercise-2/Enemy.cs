2316909be3a8c196bca7f4fd00cdc895b1fb1655
Emma Lindegaard Poulsen <emmalp@Emmas-MacBook-Pro.local>
2019-03-14 10:55:06 +0100

    Added vec2F to Enemy in task 2.6

--- SU19-Exercises/Galaga-Exercise-2/Enemy.cs
+++ SU19-Exercises/Galaga-Exercise-2/Enemy.cs
@@ -1,13 +1,18 @@
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga_Exercise_2 {
     public class Enemy : Entity {
         private Game game;
+        // 2.6 Making a get-only property vector of type Vec2F
+        private Vec2F Vector { get; }
 
 
         public Enemy(Game game, DynamicShape shape, IBaseImage image) : base(shape, image) {
             this.game = game;
+            // 2.6 Instantiating Vector as a new Vec2F
+            Vector = shape.Position;
         }
     }
 }
